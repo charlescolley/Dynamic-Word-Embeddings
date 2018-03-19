c334643502ef2cdece8531be035bea5e54736052
Charlie <charles.colley@tufts.edu>
2018-03-19 16:12:13 -0700

    added in vectorized gradients for cooccurence loss function

--- gradients.py
+++ gradients.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import sqrt
+from scipy.special import expit
 import tensorflow as tf
 from numpy.linalg import lstsq
 
@@ -16,6 +17,179 @@ def main():
   result = sym_embedding_grad(U.flatten(), P.flatten())
   print sum(result - true_result.flatten())
   '''
+
+'''
+   compute_matvec_from_vectorized_input(array,j,t,n,d,T)
+     This function computes the matvec of the j_th word in the shared 
+     embedding, with the t_th frontal slice of the core tensor. The 
+     dimensions of the n x d shared embedding, and the T x d x d core tensor 
+     are passed in to aid in computation. This is a helper function for the 
+     gradient function produced by word_to_vec_core_gradient.
+   Input:
+     array - (array of floats)
+       the array with the vectorized shared embedding and core tensor
+     j - (int)
+       the index of the vector to compute the matvec of.
+     t - (int)
+       the index of the frontal slice of compute the matvec with. 
+     n - (int)
+       the dimension (unique word count) of the shared embedding.
+     d - (int)
+       the dimension of the embedding. 
+   Returns:
+     x - (numpy array)
+       the result of the matvec. 
+'''
+def compute_matvec_from_vectorized_input(array,j,t,n,d):
+  x = np.empty(d)
+  for i in range(d):
+
+    x[i] = array[n*d + t*d*d + i]*array[j*d]
+    #do row multiplication
+    for k in range(1,d):
+      x[i] += array[n*d + t*d*d + k*d + i]*array[j*d + k]
+
+
+'''-----------------------------------------------------------------------------
+   compute_rank_1_update_from_vectorized_input(array,t,i,j,alpha,n,d)
+       This function is a helper function for the gradient computation for the 
+     word_to_vector objective function. This function takes in the shared array,
+     the indices for the associated vectors and frontal slice, and computes a 
+     rank 1 update to the frontal slice with the ith and jth word embeddings 
+     with a scaling coefficient. 
+   Input:
+     array - (array of floats)
+       the array with the vectorized shared embedding and core tensor.
+     t - (int)
+       the index of the frontal slice to be updated
+     i - (int)
+       the index of the first word embedding vector to use in the rank1 update
+     j - (int)
+       the index of the second word embedding vector to use in the rank1 update
+     alpha - (float)
+       a scaling coefficient to scale the rank 1 update by. 
+     n - (int)
+       the dimension of the initial word count (unique word count)
+     d - (int)
+       the dimension of the embedding being produced.
+-----------------------------------------------------------------------------'''
+def compute_rank_1_update_from_vectorized_input(array,t,i,j,alpha,n,d):
+  for k in range(d):
+    for l in range(d):
+      array[n*d + t*d*d +k*d + l] += alpha*array[i*d + l]*array[j*d + k]
+
+
+'''-----------------------------------------------------------------------------
+   word_to_vec_core_gradient()
+       This function takes in a list of co-occurance matrices and a 
+     dictionary with the word counts, a dimension, and a negative sampling 
+     rate and returns a function which will compute the gradient for the loss 
+     function. The objectives being learned are the shared d dimensional 
+     embedding and each of the frontal slices of the d x d x T core tensor. 
+   Input:
+     cooccurrences - (list of sparse dok matrices) 
+       The co occurences of words in the text corpus
+     word_counts - (list of dictionaries)
+       a list of dictionaries linking the index of a given word in a given 
+       time slice to the number of times the word shows up in that time 
+       slice. for each of the t dictionaries keys are the indexes, and the 
+       values are the number of times the word corersponding to that index shows
+       up in the t_th corpus.  
+     k - (int)
+       the negative sampling rate to use
+     d - (int)
+       the dimension of the embedding to be learned. 
+   Returns:
+     gradient - (function which takes in an M length array 
+                 and returns an M length array)
+       scipy.optimize takes in vectorized functions, so though the 
+       optimization function will find a n x d array and T, d x d frontal 
+       slices of the core tensor, everything will be vectorized. The first nd 
+       elements of the input will correspond to the shared embedding, and the 
+       next t sets of dd elements will correspond to each frontal slice of 
+       the core tensor.  
+-----------------------------------------------------------------------------'''
+def word_to_vec_core_gradient(cooccurences, word_counts, k, d):
+
+   T = len(cooccurences)
+   n = cooccurences[0].shape[0]
+
+   total_counts = np.zeros(T)
+   #compute all the total word counts for each time slice for normalization
+   for t in range(T):
+     for val in word_counts[t].itervalues():
+       total_counts[t] += val
+
+
+   def gradient(vectorized_input):
+
+     #initialize empty array
+     output = np.zeros(n*d + T*d*d)
+
+     #compute shared embedding gradient
+     for i in range(n):
+       for t in range(T):
+
+         B_tw_i = compute_matvec_from_vectorized_input(vectorized_input,i,t,n,d)
+
+         scaling_factor = 0
+         #compute j \neq i
+         for j in range(i):
+           scaling_factor = cooccurences[t][i,j]
+           scaling_factor -=  \
+           ((k * word_counts[t][i] * word_counts[t][j])/total_counts[t] + \
+           cooccurences[t][i,j])*expit(np.dot(vectorized_input[j*d:(j+1)*d],B_tw_i))
+
+           output[d*i:(i+1)*d] += \
+             scaling_factor*compute_matvec_from_vectorized_input(
+               vectorized_input,j,t,n,d)
+
+         for j in range(i+1,n):
+           scaling_factor = cooccurences[t][i, j]
+           scaling_factor -= \
+             ((k * word_counts[t][i] * word_counts[t][j]) / total_counts[t] + \
+              cooccurences[t][i, j]) * expit(
+               np.dot(vectorized_input[j * d:(j + 1) * d], B_tw_i))
+
+           output[d * i:(i + 1) * d] += \
+             scaling_factor * compute_matvec_from_vectorized_input(
+               vectorized_input,j,t,n,d)
+
+         #compute j == i
+           scaling_factor = cooccurences[t][i,i]
+           scaling_factor -= \
+             ((k * word_counts[t][i]**2 / total_counts[t]) + \
+              cooccurences[t][i, j]) * expit(
+               np.dot(vectorized_input[i * d:(i + 1) * d], B_tw_i))
+
+           output[d * i:(i + 1) * d] += scaling_factor * B_tw_i
+
+     #compute the frontal slices
+     for t in range(T):
+       for i in range(n):
+         B_tw_i = compute_matvec_from_vectorized_input(vectorized_input,i,t,n,d)
+         for j in range(n):
+           scaling_factor = cooccurences[t][i,j]
+           scaling_factor -= \
+             ((k * word_counts[t][i]**2 / total_counts[t]) + \
+              cooccurences[t][i, j]) * expit(
+               np.dot(vectorized_input[j * d:(j + 1) * d], B_tw_i))
+
+           compute_rank_1_update_from_vectorized_input(vectorized_input,t,i,
+                                                       j,scaling_factor,n,d)
+           
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 '''-----------------------------------------------------------------------------
